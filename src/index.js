f23d2bf9892f6f709b1bfc87785d899d3daa946c
Derek Shoemaker <shoemakerdr@gmail.com>
2017-10-31 21:39:26 -0500

    Added react-router and Nav/Footer components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Nav from './components/Nav';
+import Footer from './components/Footer'
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+    return (
+        <div className='Root'>
+            <Router>
+                <div>
+                    <Nav />
+                    <Route exact path='/' component={App} />
+                    <Footer />
+                </div>
+            </Router>
+        </div>
+    )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
