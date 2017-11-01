142931a1eb42ac94dbdf35c8249fab694dccb6b3
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-01 13:17:40 -0500

    Changed link route names

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,9 +7,9 @@ const Nav = props => {
         <div className='Nav'>
             <ul>
                 <li><Link to='/'>Home</Link></li>
-                <li><Link to='/all-polls'>All Polls</Link></li>
-                <li><Link to='/new-poll'>New Poll</Link></li>
-                <li><Link to='/user-polls'>My Polls</Link></li>
+                <li><Link to='/allpolls'>All Polls</Link></li>
+                <li><Link to='/newpoll'>New Poll</Link></li>
+                <li><Link to='/userpolls'>My Polls</Link></li>
                 <li><Link to='/login'>Login</Link></li>
                 <li><Link to='/signup'>Sign Up</Link></li>
                 <li><Link to='/signout'>Sign Out</Link></li>
