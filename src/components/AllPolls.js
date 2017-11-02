cc910c67584faa838f391eb9fc4ec4b9c0ce1936
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-01 20:00:24 -0500

    Added starting structure for AllPolls component.

--- src/components/AllPolls.js
+++ src/components/AllPolls.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import './Component.css'
+import React, { Component } from 'react'
+// import './Component.css'
 
-const Component = props => {
-    return (
-        <div className='Component'>
-            {'Some text'}
-        </div>
-    )
+const initialState = {
+            polls: [
+                {poll_id: 1, title: 'What is your favorite color?'},
+                {poll_id: 2, title: 'What is your favorite animal?'},
+                {poll_id: 3, title: 'What is your favorite food?'},
+            ]
+        }
+
+class AllPolls extends Component {
+    constructor () {
+        super()
+        this.state = { ...initialState }
+    }
+
+    render () {
+        const { polls } = this.state
+        return (
+            <div className='AllPolls'>
+                <ul>
+                {polls.map(poll => <li key={poll.poll_id}>{poll.poll_id}. {poll.title}</li>)}
+                </ul>
+            </div>
+        )
+    }
 }
 
-export default Component
+export default AllPolls
