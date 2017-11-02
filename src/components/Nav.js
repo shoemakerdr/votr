f67a4d60a60ed253f8ec456a33b706d136e210b4
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-01 21:03:54 -0500

    Changed AllPolls link to /polls

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,7 @@ const Nav = props => {
         <div className='Nav'>
             <ul>
                 <li><Link to='/'>Home</Link></li>
-                <li><Link to='/allpolls'>All Polls</Link></li>
+                <li><Link to='/polls'>All Polls</Link></li>
                 <li><Link to='/newpoll'>New Poll</Link></li>
                 <li><Link to='/userpolls'>My Polls</Link></li>
                 <li><Link to='/login'>Login</Link></li>
