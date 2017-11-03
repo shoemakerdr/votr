54e8b9391eb78a4809b50c01ef6f2e1f22e56ad3
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-02 20:26:55 -0500

    Added routes for newpolls, userpage, register, login, and homepage.

--- src/App.js
+++ src/App.js
@@ -3,30 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import Footer from './components/Footer'
+import HomePage from './components/HomePage'
 import AllPolls from './components/AllPolls'
 import Poll from './components/Poll'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Register from './components/Register'
+import Login from './components/Login'
+import NewPoll from './components/NewPoll'
+import UserPage from './components/UserPage'
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-            <Router>
-                <div>
-                    <Nav />
-                    <Switch>
-                        <Route exact path='/polls' component={AllPolls} />
-                        <Route path='/polls/:poll_id' component={Poll} />
-                    </Switch>
-                    <Footer />
-                </div>
-            </Router>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Router>
+                    <div>
+                        <Nav />
+                        <Switch>
+                            <Route exact path='/' component={HomePage} />
+                            <Route exact path='/polls' component={AllPolls} />
+                            <Route path='/newpoll' component={NewPoll} />
+                            <Route path='/polls/:poll_id' component={Poll} />
+                            <Route exact path='/users' render={() =>  (
+                                <Redirect to='/' />
+                            )}/>
+                            <Route path='/users/:user_id' component={UserPage} />
+                            <Route path='/register' component={Register} />
+                            <Route path='/login' component={Login} />
+                        </Switch>
+                        <Footer />
+                    </div>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
