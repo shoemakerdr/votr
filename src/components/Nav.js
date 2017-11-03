1d4db30bf5a290adb974222e368a0599233aef32
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-02 20:30:23 -0500

    Changed link to for mypolls and sign up

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,9 +9,9 @@ const Nav = props => {
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/polls'>All Polls</Link></li>
                 <li><Link to='/newpoll'>New Poll</Link></li>
-                <li><Link to='/userpolls'>My Polls</Link></li>
+                <li><Link to='/users/1'>My Polls</Link></li>
                 <li><Link to='/login'>Login</Link></li>
-                <li><Link to='/signup'>Sign Up</Link></li>
+                <li><Link to='/register'>Sign Up</Link></li>
                 <li><Link to='/signout'>Sign Out</Link></li>
             </ul>
         </div>
