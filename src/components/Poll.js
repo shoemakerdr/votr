7025b13e2da891723acd5d091642bd5dbf4bc22d
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-04 23:58:47 -0500

    Added fetch call for poll api

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react'
 // import './Component.css'
 
 class Poll extends Component {
+    constructor () {
+        super()
+        this.state = {
+            poll: null
+        }
+    }
+
+    componentDidMount () {
+        this.fetchPoll().then(poll => this.setState({poll: poll}))
+    }
+
+    fetchPoll () {
+        return fetch(`http://localhost:8000/api/polls/${this.props.match.params.poll_id}`)
+            .then(data => data.json())
+    }
+
     render () {
         return (
             <div className='Poll'>
-                Poll #{this.props.match.params.poll_id}
+            { this.state.poll &&
+                <div>
+                    <p>Poll: {this.state.poll.title}</p>
+                    {this.state.poll.options.map(option => {
+                        return (
+                            <p key={option.id}>Option {option.name} got {option.votes} votes.</p>
+                        )
+                    })}
+                </div>
+            }
             </div>
         )
     }
