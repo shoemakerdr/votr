6868648c7ac2223f4dfcaebfd04bce8178cc048b
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-05 22:02:02 -0600

    Changed some routes to render with passed down props. Added 404 NotFoundPage when path match not found.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,108 @@
-import React, { Component } from 'react';
-import './App.css';
-import Nav from './components/Nav';
+import React, { Component } from 'react'
+import './App.css'
+import Nav from './components/Nav'
 import Footer from './components/Footer'
 import HomePage from './components/HomePage'
-import AllPolls from './components/AllPolls'
+import PollList from './components/PollList'
 import Poll from './components/Poll'
 import Register from './components/Register'
 import Login from './components/Login'
 import NewPoll from './components/NewPoll'
-import UserPage from './components/UserPage'
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
+import UserPollList from './components/UserPollList'
+import Signout from './components/Signout'
+import NotFoundPage from './components/NotFoundPage'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 class App extends Component {
+    constructor () {
+        super()
+        this.api = 'http://localhost:8000/api'
+        this.logInUser = this.logInUser.bind(this)
+        this.signOutUser = this.signOutUser.bind(this)
+        this.state = {
+            userInfo: {
+                isLoggedIn: true,
+                userId: 1,
+                username: 'derek'
+            }
+        }
+    }
+
+    logInUser () {
+        this.setState({
+            userInfo: {
+                isLoggedIn: true,
+                userId: 1,
+                username: 'derek'
+            }
+        })
+    }
+
+    signOutUser () {
+        this.setState({
+            userInfo: {
+                isLoggedIn: false,
+                userId: null,
+                username: ''
+            }
+        })
+    }
+
     render() {
         return (
             <div className="App">
                 <Router>
                     <div>
-                        <Nav />
+                        <Nav userInfo={this.state.userInfo} />
                         <Switch>
                             <Route exact path='/' component={HomePage} />
-                            <Route exact path='/polls' component={AllPolls} />
-                            <Route path='/newpoll' component={NewPoll} />
-                            <Route path='/polls/:poll_id' component={Poll} />
-                            <Route exact path='/users' render={() =>  (
-                                <Redirect to='/' />
-                            )}/>
-                            <Route path='/users/:user_id' component={UserPage} />
-                            <Route path='/register' component={Register} />
-                            <Route path='/login' component={Login} />
+                            <Route exact path='/polls' render={props => (
+                                <PollList {...props} api={this.api} />
+                            )} />
+                            <Route path='/newpoll' render={props => (
+                                <NewPoll
+                                    {...props}
+                                    api={this.api}
+                                    userInfo={this.state.userInfo}
+                                />
+                            )} />
+                            <Route path='/polls/:poll_id' render={props => (
+                                <Poll
+                                    {...props}
+                                    api={this.api}
+                                    userInfo={this.state.userInfo}
+                                />
+                            )} />
+                            <Route path='/users/:user_id' render={props => (
+                                <UserPollList
+                                    {...props}
+                                    api={this.api}
+                                    userInfo={this.state.userInfo}
+                                />
+                            )} />
+                            <Route path='/register' render={props => (
+                                <Register {...props} api={this.api} />
+                            )} />
+                            <Route path='/login' render={props => (
+                                <Login
+                                    {...props}
+                                    api={this.api}
+                                    logInUser={this.logInUser}
+                                />
+                            )} />
+                            <Route path='/signout' render={() =>
+                                <Signout signOutUser={this.signOutUser} />
+                            } />
+                            <Route path='/' render={() =>
+                                <NotFoundPage />
+                            } />
                         </Switch>
                         <Footer />
                     </div>
                 </Router>
             </div>
-        );
+        )
     }
 }
 
-export default App;
+export default App
