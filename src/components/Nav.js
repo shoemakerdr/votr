e539087b895146e99ccf8da9c7cc67a0f4673906
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-05 21:57:32 -0600

    Made some Link render conditionally based on whether user is logged in

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,16 +3,17 @@ import { Link } from 'react-router-dom'
 // import './Nav.css'
 
 const Nav = props => {
+    const { isLoggedIn, userId, username } = props.userInfo
     return (
         <div className='Nav'>
             <ul>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/polls'>All Polls</Link></li>
-                <li><Link to='/newpoll'>New Poll</Link></li>
-                <li><Link to='/users/1'>My Polls</Link></li>
-                <li><Link to='/login'>Login</Link></li>
-                <li><Link to='/register'>Sign Up</Link></li>
-                <li><Link to='/signout'>Sign Out</Link></li>
+                {isLoggedIn && <li><Link to='/newpoll'>New Poll</Link></li>}
+                {isLoggedIn && <li><Link to={`/users/${userId}`}>{username}'s Polls</Link></li>}
+                {isLoggedIn && <li><Link to='/signout'>Sign Out</Link></li>}
+                {isLoggedIn || <li><Link to='/login'>Login</Link></li>}
+                {isLoggedIn || <li><Link to='/register'>Sign Up</Link></li>}
             </ul>
         </div>
     )
