a5f92e37c6ed30e0f1007d62588fa7fa8d034230
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-08 00:55:39 -0600

    Added form elements and logic for multiple options.

--- src/components/NewPoll.js
+++ src/components/NewPoll.js
@@ -1,12 +1,67 @@
-import React from 'react'
-// import './NewPoll.css'
+import React, { Component } from 'react'
 
-const NewPoll = props => {
-    return (
-        <div className='NewPoll'>
-            {'CREATE A NEW POLL'}
-        </div>
-    )
+class NewPoll extends Component {
+    constructor (props) {
+        super()
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.addOption = this.addOption.bind(this)
+        this.changeOption = this.changeOption.bind(this)
+        this.defaultState = {
+            numberOfOptions: 2,
+            inputValue: '',
+            options: ['','']
+        }
+        this.state = this.defaultState
+    }
+
+    handleChange (event) {
+        this.setState({inputValue: event.target.value})
+    }
+
+    handleSubmit (event) {
+        console.log(this.state)
+        this.setState(this.defaultState)
+        event.preventDefault()
+    }
+
+    addOption (event) {
+        event.preventDefault()
+        const newOptions = this.state.options.map(x => x)
+        newOptions.push('')
+        this.setState({
+            numberOfOptions: this.state.numberOfOptions++,
+            options: newOptions
+        })
+    }
+
+    changeOption (event) {
+        const index = event.target.name
+        const newOptions = this.state.options.map((x,i) => String(i) === index ? event.target.value : x)
+        this.setState({ options: newOptions })
+    }
+
+    render () {
+        return (
+            <form className='NewPoll' onSubmit={this.handleSubmit}>
+                <label>
+                    Title:
+                    <input type="text" value={this.state.inputValue} onChange={this.handleChange} />
+                </label>
+                <label>
+                    Options:
+                    { this.state.options.map((option, i) => {
+                        return (
+                            <input type="text" key={i} name={`${i}`} value={this.state.options[i]} onChange={this.changeOption} />
+                        )
+                    })}
+                    <button onClick={this.addOption}>Add option</button>
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        )
+    }
 }
 
 export default NewPoll
+
