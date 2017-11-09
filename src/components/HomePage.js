70de8bb9471743dc2b62890caa7341c0d5597fde
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-09 22:23:48 +0000

    Added logo and styling

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import votrLogo from 'votr-logo.png'
 // import './HomePage.css'
+
+const styles = {
+    logo: {
+        height: '200px',
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    }
+}
 
 const HomePage = props => {
     return (
         <div className='HomePage'>
-            {'WELCOME TO VOTR!'}
+            <img src={votrLogo} alt='votr logo' style={styles.logo} />
+            <h1>WELCOME TO VOTR!</h1>
         </div>
     )
 }
