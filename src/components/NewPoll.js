bb1b1394249efa093b03226a89d2cdf97646c4f5
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-09 22:27:20 +0000

    Added canAddOptions boolean in state to disable Add Options button when there are empty options inputs. Added hasNoEmptyOptions method for checking.

--- src/components/NewPoll.js
+++ src/components/NewPoll.js
@@ -7,9 +7,11 @@ class NewPoll extends Component {
         this.handleSubmit = this.handleSubmit.bind(this)
         this.addOption = this.addOption.bind(this)
         this.changeOption = this.changeOption.bind(this)
+        this.hasNoEmptyOptions = this.hasNoEmptyOptions.bind(this)
         this.defaultState = {
             title: '',
-            options: ['','']
+            options: ['',''],
+            canAddOption: false
         }
         this.state = this.defaultState
     }
@@ -23,22 +25,35 @@ class NewPoll extends Component {
         this.setState(this.defaultState)
         event.preventDefault()
     }
+    
+    hasNoEmptyOptions () {
+        const { options } = this.state
+        for (let option of options) {
+            if (option.length === 0)
+                return false
+        }
+        return true
+    }
 
     addOption (event) {
         event.preventDefault()
-        const newOptions = this.state.options.map(x => x)
-        newOptions.push('')
-        this.setState(prevState => {
-            return {
+        if (this.noEmptyOptions()) {
+            const newOptions = this.state.options.map(x => x)
+            newOptions.push('')
+            this.setState({
+                canAddOption: this.hasNoEmptyOptions(),
                 options: newOptions
-            }
-        })
+            })
+        }
     }
 
     changeOption (event) {
         const index = event.target.name
         const newOptions = this.state.options.map((x,i) => String(i) === index ? event.target.value : x)
-        this.setState({ options: newOptions })
+        this.setState({
+            canAddOption: this.hasNoEmptyOptions(),
+            options: newOptions
+        })
     }
 
     render () {
@@ -46,16 +61,32 @@ class NewPoll extends Component {
             <form className='NewPoll' onSubmit={this.handleSubmit}>
                 <label>
                     Title:
-                    <input type="text" value={this.state.title} onChange={this.handleChange} />
+                    <input
+                        type="text"
+                        value={this.state.title}
+                        onChange={this.handleChange}
+                    />
                 </label>
                 <label>
                     Options:
                     { this.state.options.map((option, i) => {
                         return (
-                            <input type="text" key={i} name={`${i}`} value={this.state.options[i]} onChange={this.changeOption} />
+                            <input 
+                                type="text"
+                                key={i}
+                                name={`${i}`}
+                                value={this.state.options[i]}
+                                onChange={this.changeOption} 
+                            />
                         )
                     })}
-                    <button type='button' onClick={this.addOption}>Add option</button>
+                    <button
+                        type='button'
+                        disabled={this.state.canAddOption}
+                        onClick={this.addOption}
+                    >
+                        Add option
+                    </button>
                 </label>
                 <input type="submit" value="Submit" />
             </form>
