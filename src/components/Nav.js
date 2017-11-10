3f4940a6084862eb563069cc1c114cc013355993
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-10 13:44:49 -0600

    Added styling to nav links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,27 +1,34 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import votrLogoNav from '../images/votr-logo-nav.png'
 
 const styles = {
     navbar: {
-        padding: '24px',
-        marginBottom: '16px',
+        padding: '8px',
+        marginBottom: '8px',
         backgroundColor: '#050D58',
         display: 'flex',
         justifyContent: 'space-between',
+        alignItems: 'center',
     },
     link: {
-        marginRight: '12px',
-        marginLeft: '12px',
+        marginRight: '8px',
+        marginLeft: '8px',
         color: 'white',
         textDecoration: 'none',
         fontSize: '20px'
     },
     active: {
-        borderBottom: '2px solid red'
+        borderBottom: '2px solid red',
+        alignSelf: 'center',
     },
     logo: {
-        height: '80px'
+        height: '100px',
+        marginLeft: '16px',
+    },
+    main: {
+        display: 'flex',
+        alignItems: 'center',
     }
 }
 
@@ -38,20 +45,20 @@ const Nav = props => {
     const { isLoggedIn, userId, username } = props.userInfo
     return (
         <div className='Nav' style={styles.navbar}>
-            <div className='Nav--home-polls'>
-                <img src={votrLogoNav} alt='votr' style={styles.logo} />
+            <Link to='/'><img src={votrLogoNav} alt='votr' style={styles.logo} /></Link>
+            <div className='Nav--main' style={styles.main}>
                 <StyledNavLink exact to='/'>Home</StyledNavLink>
                 <StyledNavLink to='/polls'>Polls</StyledNavLink>
+                {isLoggedIn && <div className='Nav--authenticated'>
+                    <StyledNavLink to='/newpoll'>Create Poll</StyledNavLink>
+                    <StyledNavLink to={`/users/${userId}`}>Welcome {username}!</StyledNavLink>
+                    <StyledNavLink to='/signout'>Sign Out</StyledNavLink>
+                </div>}
+                {!isLoggedIn && <div className='Nav--unauthenticated'>
+                    <StyledNavLink to='/login'>Login</StyledNavLink>
+                    <StyledNavLink to='/register' >Sign Up</StyledNavLink>
+                </div>}
             </div>
-            {isLoggedIn && <div className='Nav--authenticated'>
-                <StyledNavLink to='/newpoll'>Create Poll</StyledNavLink>
-                <StyledNavLink to={`/users/${userId}`}>Welcome {username}!</StyledNavLink>
-                <StyledNavLink to='/signout'>Sign Out</StyledNavLink>
-            </div>}
-            {!isLoggedIn && <div className='Nav--unauthenticated'>
-                <StyledNavLink to='/login'>Login</StyledNavLink>
-                <StyledNavLink to='/register' >Sign Up</StyledNavLink>
-            </div>}
         </div>
     )
 }
