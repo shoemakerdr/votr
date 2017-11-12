edd7e78d77a65b2dda4b9a1850b133a465b64e83
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-12 14:19:37 -0600

    Changed tagline

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import votrLogo from '../images/votr-logo.png'
-// import './HomePage.css'
+import './styles/HomePage.css'
 
 const styles = {
     logo: {
@@ -14,7 +14,7 @@ const HomePage = props => {
     return (
         <div className='HomePage'>
             <img src={votrLogo} alt='votr logo' style={styles.logo} />
-            <h1>WELCOME TO VOTR!</h1>
+            <h1 className='HomePage--tagline'>Exercise your right to Votr</h1>
         </div>
     )
 }
