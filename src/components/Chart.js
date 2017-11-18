461bee9f19cc0a611d50f8ff579871769dd99738
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-18 16:37:45 -0600

    Imported Doughnut component from react-chartjs-2, colorsFromDataLength function from utils.js, and styling from ccs files. Changed Chart component to render chart.

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import { Doughnut } from 'react-chartjs-2'
+import { colorsFromDataLength } from '../utils'
+import './styles/Chart.css'
 
-const Chart = props =>
-    <div className='Chart'>
-        { props.options.length && props.options.map(option => {
-            return (
-                <p key={option.id}>{option.name}: {option.votes} votes</p>
-            )
-        })}
-        { !props.options.length && 'No votes yet'}
-    </div>
+const Chart = props => {
+    const labels = props.options.map(opt => opt.name)
+    const data = props.options.map(opt => opt.votes)
+    const colors = colorsFromDataLength(props.options.length)
+    const chartData = {
+        labels: labels,
+        datasets: [{
+            data: data,
+            backgroundColor: colors,
+            hoverBackgroundColor: colors
+        }]
+    }
+    return (
+        <div className='Chart'>
+            <div>
+                <Doughnut data={chartData} />
+            </div>
+        </div>
+    )
+}
 
 export default Chart
