73eebba5fac8c90a69aceb121a2a0961430f5eb8
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-19 14:15:12 -0600

    Added css module import of styles. Added h4 and a tag around footer text.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,10 @@
 import React from 'react'
-// import './Footer.css'
-
-const styles = {
-    marginTop:'48px'
-}
+import styles from './styles/Footer.css'
 
 const Footer = props => {
     return (
-        <div style={styles} className='Footer'>
-            Coded by Derek Shoemaker
+        <div className={styles.footer}>
+            <h4>by <a href='https://github.com/shoemakerdr' className={styles.link}>Derek Shoemaker</a></h4>
         </div>
     )
 }
