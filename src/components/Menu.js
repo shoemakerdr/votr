354a8d5891c89dd1f0055f2afc5247d378516046
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-19 00:12:09 -0600

    Extracted Menu component from SliderNav and Nav.

---
+++ src/components/Menu.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import StyledNavLink from './StyledNavLink'
+
+const styles = {
+    row: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    column: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+    }
+}
+
+const Menu = props => {
+    const { isLoggedIn, username } = props.userInfo
+    const style = props.direction ? styles[props.direction] : styles.row
+    return (
+        <div style={style}>
+            <StyledNavLink exact to='/'>Home</StyledNavLink>
+            <StyledNavLink to='/polls'>Polls</StyledNavLink>
+            {isLoggedIn && <div style={style}>
+                <StyledNavLink to='/newpoll'>Create Poll</StyledNavLink>
+                <StyledNavLink to={`/users/${username}`}>Dashboard</StyledNavLink>
+                <StyledNavLink to='/signout'>Sign Out</StyledNavLink>
+            </div>}
+            {!isLoggedIn && <div style={style}>
+                <StyledNavLink to='/login'>Login</StyledNavLink>
+                <StyledNavLink to='/register' >Sign Up</StyledNavLink>
+            </div>}
+        </div>
+    )
+}
+
+export default Menu
+
