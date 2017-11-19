8ffa75896e8d965e2f3334523c04cf1b15ff9232
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-19 13:22:21 -0600

    Imported HamburgerIcon component. Wrapped Menu for additional styling based on screen width. Passed toggleSlideMenu prop to HamburgerIcon.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Menu from './Menu'
+import HamburgerIcon from './HamburgerIcon'
 import styles from './styles/Nav.css'
 import votrLogoNav from '../images/votr-logo-nav.png'
 
 const Nav = props =>
     <div className={styles.navbar}>
         <Link to='/'><img src={votrLogoNav} alt='votr' className={styles.logo} /></Link>
-        <Menu userInfo={props.userInfo}/>
+        <div className={styles.menu}>
+            <Menu userInfo={props.userInfo}/>
+        </div>
+        <HamburgerIcon toggleSlideMenu={props.toggleSlideMenu}/>
     </div>
-
 export default Nav
 
