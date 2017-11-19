0f4a986d9ea63d86e3f1880a83a26c732d94773b
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-19 00:14:34 -0600

    Removed inline styles in favor of importing css module object. Removed StyledNavLink. Extracted Menu as separate component and imported into Nav.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,67 +1,14 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import Menu from './Menu'
+import styles from './styles/Nav.css'
 import votrLogoNav from '../images/votr-logo-nav.png'
 
-const styles = {
-    navbar: {
-        padding: '8px',
-        marginBottom: '8px',
-        backgroundColor: '#050D58',
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-    },
-    link: {
-        marginRight: '8px',
-        marginLeft: '8px',
-        color: 'white',
-        textDecoration: 'none',
-        fontSize: '20px'
-    },
-    active: {
-        borderBottom: '2px solid red',
-        alignSelf: 'center',
-    },
-    logo: {
-        height: '100px',
-        marginLeft: '16px',
-    },
-    main: {
-        display: 'flex',
-        alignItems: 'center',
-    }
-}
-
-const StyledNavLink = props =>
-    <NavLink
-        {...props}
-        style={styles.link}
-        activeStyle={styles.active}
-    >
-        {props.children}
-    </NavLink>
-
-const Nav = props => {
-    const { isLoggedIn, username } = props.userInfo
-    return (
-        <div className='Nav' style={styles.navbar}>
-            <Link to='/'><img src={votrLogoNav} alt='votr' style={styles.logo} /></Link>
-            <div className='Nav--main' style={styles.main}>
-                <StyledNavLink exact to='/'>Home</StyledNavLink>
-                <StyledNavLink to='/polls'>Polls</StyledNavLink>
-                {isLoggedIn && <div className='Nav--authenticated'>
-                    <StyledNavLink to='/newpoll'>Create Poll</StyledNavLink>
-                    <StyledNavLink to={`/users/${username}`}>Dashboard</StyledNavLink>
-                    <StyledNavLink to='/signout'>Sign Out</StyledNavLink>
-                </div>}
-                {!isLoggedIn && <div className='Nav--unauthenticated'>
-                    <StyledNavLink to='/login'>Login</StyledNavLink>
-                    <StyledNavLink to='/register' >Sign Up</StyledNavLink>
-                </div>}
-            </div>
-        </div>
-    )
-}
+const Nav = props =>
+    <div className={styles.navbar}>
+        <Link to='/'><img src={votrLogoNav} alt='votr' className={styles.logo} /></Link>
+        <Menu userInfo={props.userInfo}/>
+    </div>
 
 export default Nav
 
