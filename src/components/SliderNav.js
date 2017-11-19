77bad7d2bca228da8cdf0a65cd298b3a4039da69
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-19 13:23:49 -0600

    Added onClick event to toggle slider. Added wrapper around slider.

--- src/components/SliderNav.js
+++ src/components/SliderNav.js
@@ -4,8 +4,10 @@ import Menu from './Menu'
 
 const SliderNav = props =>
     // shown prop is passed down when slider menu should be slide out
-    <div className={props.shown ? styles.slider : `${styles.slider} ${styles.putAway}`}>
-        <Menu userInfo={props.userInfo} direction='column'/>
+    <div onClick={props.toggleSlideMenu} className={props.shown ? styles.wrapper : styles.wrapperHidden}>
+        <div className={props.shown ? styles.slider : `${styles.slider} ${styles.putAway}`}>
+            <Menu userInfo={props.userInfo} direction='column'/>
+        </div>
     </div>
 
 export default SliderNav
