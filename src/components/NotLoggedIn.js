f36c7f46b74f53b65db59cb24783876b16b9feac
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-19 22:24:12 -0600

    Imported local module style object. Imported BackToLink to navigate to login page.

--- src/components/NotLoggedIn.js
+++ src/components/NotLoggedIn.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import BackToLink from './BackToLink'
+import styles from './styles/NotLoggedIn.css'
 
 const NotLoggedIn = props => {
     if (!props.username) {
         return (
-            <div style={{marginBottom: '16px'}}>
-                <h1>You must be logged in to use this feature.</h1>
-                <Link to='/login'><button className='button'>Click to login</button></Link>
-            </div>
+            <div className={styles.wrapper}>
+                <h1 className={styles.message}>You must be logged in to use this feature.</h1>
+                <BackToLink message='Go to Login Page' to='/login' />
+                </div>
         )
     }
     return <div></div>
