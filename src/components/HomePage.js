c5915a1352e6581a7860eacf3febf864480bb846
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-19 22:01:17 -0600

    Imported BackToLink to navigate to /polls page.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import votrLogo from '../images/votr-logo.png'
+import BackToLink from './BackToLink'
 import styles from './styles/HomePage.css'
 
 const HomePage = props => {
@@ -7,6 +8,7 @@ const HomePage = props => {
         <div className={styles.page}>
             <img src={votrLogo} alt='votr logo' className={styles.logo} />
             <h1 className={styles.tagline}>Exercise your right to Votr</h1>
+            <BackToLink to='/polls' arrow='right' message='Vote in the latest polls' />
         </div>
     )
 }
