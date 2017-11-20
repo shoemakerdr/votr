e5454f01579e5da5cb8843b1c0ad39618043712f
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-19 18:08:53 -0600

    Imported styles objects from css module. Assigned styles to classes on component.

--- src/components/PollList.js
+++ src/components/PollList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import styles from './styles/PollList.css'
 import votrApi from '../votrApi'
 
 const initialState = {
@@ -23,19 +24,20 @@ class PollList extends Component {
     render () {
         const { polls } = this.state
         return (
-            <div className='AllPolls'>
-            {this.state.polls.length ?
-                <div>
-                    {polls.map(poll => {
-                        const {title, poll_id} = poll
-                        return (
-                            <div key={poll_id}>
-                                <Link to={`/polls/${poll_id}`}>{title}</Link>
-                            </div>
-                        )
-                    })}
-                </div>
-                : 'No polls to show'}
+            <div className={styles.list}>
+                <h1 className={styles.title}>Current Polls</h1>
+                {this.state.polls.length ?
+                    <div>
+                        {polls.map(poll => {
+                            const {title, poll_id} = poll
+                            return (
+                                <div className={styles.listItem} key={poll_id}>
+                                    <Link className={styles.link} to={`/polls/${poll_id}`}>{title}</Link>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    : 'No polls to show'}
             </div>
         )
     }
